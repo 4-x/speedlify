f9cb78d94585ef5abf38af3015edbead09bd88b7
Alexander Shoup <alexshoup@gmail.com>
2023-05-02 13:43:40 -0500

    clean up

---
+++ _data/sites/alexandershoup.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: "alexandershoup.com",
+  description: "Alexander Shoup dot com",
+  skip: false,
+  options: {
+    frequency: 60 * 23,
+    freshChrome: "site",
+  },
+  urls: [
+    "https://alexandershoup.com/",
+    "https://alexandershoup.com/colophon",
+
+  ],
+};
